250153114c511a29118fc6236efbf0a5eaf39fdf
WinDows <WinDows@SYEDTAJMULHUSSAIN>
2024-05-30 21:30:12 +0530

    first prog

--- c38/src/com/tns/day1/FirstProgram.java
+++ c38/src/com/tns/day1/FirstProgram.java
@@ -2,8 +2,8 @@ package com.tns.day1;
 
 public class FirstProgram
 { 
-public static void main(String[] args)
+public static void main(String[] args) 
 { 
 System.out.println("Hello World"); 
 } 
-}
+} 
